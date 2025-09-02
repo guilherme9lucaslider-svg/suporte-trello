97d7194fd6585ecabca1c71581e424513c1e963f
guilherme9lucaslider-svg <guilherme9lucaslider@gmail.com>
2025-09-02 00:49:13 -0300

    Atualização

--- templates/update.ps1
+++ templates/update.ps1
@@ -2,19 +2,19 @@ cd D:\Projetos\suporte-trello
 
 git init 
 
-# garante que estÃ¡ na branch main 
+# garante que está na branch main 
 git branch -M main 
 
 # adiciona todos os arquivos novos e modificados 
 git add . 
 
 # cria um commit 
-git commit -m "AtualizaÃ§Ã£o" 
+git commit -m "Atualização" 
 
-# garante que o remoto estÃ¡ configurado 
+# garante que o remoto está configurado 
 git remote remove origin 2>$null git remote add origin https://github.com/guilherme9lucaslider-svg/suporte-trello.git 
 
-# envia para o GitHub na branch main (forÃ§ando a substituir o conteÃºdo antigo) 
+# envia para o GitHub na branch main (forçando a substituir o conteúdo antigo) 
 git push -u origin main --force 
 
 exit
